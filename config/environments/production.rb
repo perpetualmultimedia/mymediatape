c71172488c507c2ab1692f0d216470f59c9250b8
Colton Hagan <me@perpetualsite.com>
2016-11-06 16:40:10 -0700

    s3 images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
@@ -85,5 +85,7 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   }
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
 }
 end
