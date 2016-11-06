089572338b3dd3c6bd8d73975bc5d9ee778f0cd7
Colton Hagan <me@perpetualsite.com>
2016-11-06 16:46:17 -0700

    s3 images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   }
-    :s3_host_name => 'mymediatape.s3.amazonaws.com'
+  s3_host_name: 'mymediatape.s3.amazonaws.com'
 }
 end
