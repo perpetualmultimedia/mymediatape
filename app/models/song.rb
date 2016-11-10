2a000262be54623f7bc1e915558ad00e7982ad75
Colton Hagan <me@perpetualsite.com>
2016-11-10 00:44:06 -0700

    tracks listing

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,5 @@ class Song < ActiveRecord::Base
   has_attached_file :audio_file
   validates_attachment_content_type :audio_file, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
   default_scope { order("track_number") }
-
+  
 end
