8e20cbd5ae2d5eb7c2e98f6b26bfb5f00e34455e
Colton Hagan <me@perpetualsite.com>
2017-05-27 15:46:47 -0700

    facelift

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,8 +5,14 @@ class Album < ActiveRecord::Base
   include PublicActivity::Model
   tracked only: [:update, :create], :owner => :band
   has_many :album_comments, dependent: :destroy
-  accepts_nested_attributes_for :songs, allow_destroy: true, :reject_if => lambda { |a| a[:title].blank? }
+  accepts_nested_attributes_for :album_comments, allow_destroy: true, :reject_if => lambda { |a| a[:body].blank? }
+  accepts_nested_attributes_for :songs, allow_destroy: true, reject_if: :comment_rejectable?
   has_attached_file :album_art, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :album_art, content_type: /\Aimage\/.*\z/
   default_scope { order(released: :desc) }
+
+  private
+   def comment_rejectable?(att)
+  att['body'].blank? 
+ end
 end
